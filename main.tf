ded153aa5d8c6bba6d8743794a8135fe224c8caa
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-02 10:32:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "test_bucket_good" {
-  name                        = "test-bucket-good"
+  name                        = "test-bucket-good-pankhuri"
   location                    = "EU"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 }
 resource "google_storage_bucket" "test_bucket_bad" {
-  name                        = "test-bucket-bad"
+  name                        = "test-bucket-bad-pankhuri"
   location                    = "EU"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = false
