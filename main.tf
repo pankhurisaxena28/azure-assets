1c8d175e9fb7c950c0be0415128b00a39a80c6a0
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-02 14:32:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "google" {
 }
 provider "azurerm" {
     subscription_id = "5b87efeb-4ef9-40ca-8cb4-cfa6d2cd0351"
+    tenant_id = "a162027e-b677-4249-b759-a9e6efa41922"
     features {
     }
 }
