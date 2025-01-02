7ad1ec657b83fe96db538d49121819655134740c
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-02 14:56:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,18 +9,6 @@ provider "azurerm" {
     }
 }
 
-resource "google_storage_bucket" "test_bucket_good" {
-  name                        = "test-bucket-good-pankhuri"
-  location                    = "EU"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-resource "google_storage_bucket" "test_bucket_bad" {
-  name                        = "test-bucket-bad-pankhuri"
-  location                    = "EU"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = false
-}
 
 
 resource "azurerm_network_security_group" "example" {
