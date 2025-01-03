335b1f2674df7427ea1ddd056693df79314b0391
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-03 11:07:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,10 +20,6 @@ resource "azurerm_network_security_group" "example" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
   }
 
   tags = {
