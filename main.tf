30b005edd6d7c47ece55afb037f987913abbbc1e
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-03 11:05:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,17 @@ resource "azurerm_network_security_group" "example" {
   location            = "westeurope"
   resource_group_name = "iac-rego-testing"
 
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
   tags = {
     environment = "Production"
