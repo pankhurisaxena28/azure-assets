2970dc196e1c707c6d241518463359e465cd0476
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-10 09:39:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "google" {
   project = "pankhuri-test-proj-436319"
   region  = "us-central1"
 }
-
 provider "azurerm" {
     subscription_id = "5b87efeb-4ef9-40ca-8cb4-cfa6d2cd0351"
     tenant_id = "a162027e-b677-4249-b759-a9e6efa41922"
@@ -15,4 +14,26 @@ resource "google_storage_bucket" "test_bucket_bad" {
   location                    = "ASIA"
   storage_class               = "ARCHIVE"
   uniform_bucket_level_access = false
+}
+
+resource "azurerm_network_security_group" "exampleSG" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = "westeurope"
+  resource_group_name = "iac-rego-testing"
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = 8080
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
