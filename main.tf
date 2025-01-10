1abeeefa9474bfa7cdc23711a6f08b3c55a2dc8c
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-10 09:30:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ provider "google" {
   region  = "us-central1"
 }
 
+provider "azurerm" {
+    subscription_id = "5b87efeb-4ef9-40ca-8cb4-cfa6d2cd0351"
+    tenant_id = "a162027e-b677-4249-b759-a9e6efa41922"
+    features {
+    }
+}
+
 resource "google_storage_bucket" "test_bucket_bad" {
   name                        = "test-bucket-bad"
   location                    = "ASIA"
