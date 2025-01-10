ffa18709f3e43b9813744ff7f2fc8ed2c3237b71
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-10 14:16:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,14 @@ provider "azurerm" {
 
 resource "google_storage_bucket" "test_bucket" {
   name                        = "test-bucket"
-  location                    = "ASIA"
-  storage_class               = "ARCHIVE"
-  uniform_bucket_level_access = false
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  }
+
 }
 
 resource "azurerm_network_security_group" "test_network_security_group" {
@@ -30,7 +35,7 @@ resource "azurerm_network_security_group" "test_network_security_group" {
     source_port_range          = 8080
     destination_port_range     = "*"
     source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    destination_address_prefix = "8000"
   }
 
   tags = {
