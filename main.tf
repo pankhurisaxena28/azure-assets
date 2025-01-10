d6f35a712eef3b9e9a61f197e8fbfd68a32df66b
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-10 09:32:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,10 @@ provider "azurerm" {
 resource "google_storage_bucket" "test_bucket_bad" {
   name                        = "test-bucket-bad"
   location                    = "ASIA"
+  parent                      = "pankhuri-test-proj-436319"
   storage_class               = "ARCHIVE"
   uniform_bucket_level_access = false
+
 
 
 }
