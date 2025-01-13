2fa47d0b0135a708449a3257d8f7490b140fd2d5
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-01-13 09:33:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ provider "azurerm" {
     features {
     }
 }
+
+resource "google_storage_bucket" "test_bucket_bad" {
+  name                        = "test-bucket-bad"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  }
+}
