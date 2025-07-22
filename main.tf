4ff4604ee88174d9cc80344e1e43a8f34dfb0723
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-07-22 21:18:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "google_storage_bucket" "test_bucket_pankhuri" {
   name                        = "test-bucket-pankhuri"
   location                    = "EU"
   storage_class               = "STANDARD"
-  uniform_bucket_level_access = false
+  uniform_bucket_level_access = true
   logging {
     log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
     log_object_prefix = "tf-logs/"             // Optional prefix for better structure
@@ -32,10 +32,10 @@ resource "azurerm_network_security_group" "test_network_security_group_pankhuri"
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = 8080
+    source_port_range          = 8000
     destination_port_range     = "*"
     source_address_prefix      = "*"
-    destination_address_prefix = "8000"
+    destination_address_prefix = "8080"
   }
 
   tags = {
