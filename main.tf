a6cd7927b30ba8967b9a444ec073cd71eae1d75f
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-07-22 21:10:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,17 @@ provider "azurerm" {
     }
 }
 
+resource "google_storage_bucket" "test_bucket_pankhuri" {
+  project                     = "pankhuri-test-proj-436319"
+  name                        = "test-bucket-pankhuri"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  }
+}
 
 resource "azurerm_network_security_group" "test_network_security_group_pankhuri" {
   name                = "test-network-security-group-pankhuri"
