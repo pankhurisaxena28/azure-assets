c65bb2ac4df5d0ff4a94881cdfbdf532af535a66
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-07-25 09:15:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "google_storage_bucket" "test_bucket_pankhuri" {
   name                        = "test-bucket-pankhuri"
   location                    = "EU"
   storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
   logging {
     log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
     log_object_prefix = "tf-logs/"             // Optional prefix for better structure
