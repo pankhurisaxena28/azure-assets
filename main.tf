4ab6c242f1f4f16eceda9936e809ccb929167041
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-07-28 11:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,20 +13,16 @@ provider "azurerm" {
 resource "google_storage_bucket" "test_bucket_pankhuri" {
   project                     = "pankhuri-test-proj-436319"
   name                        = "test-bucket-pankhuri"
-  location                    = "EU"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  }
+  location                    = "ASIA"
+  storage_class               = "ARCHIVE"
+  uniform_bucket_level_access = false
 }
 
 resource "google_bigquery_dataset" "default" {
   dataset_id                  = "mydataset"
   friendly_name               = "test"
   description                 = "This is a test description"
-  location                    = "us-east-2"
+  location                    = "us-east-1"
   default_table_expiration_ms = 3600000
 
   labels = {
@@ -48,7 +44,7 @@ resource "azurerm_network_security_group" "test_network_security_group_pankhuri"
     source_port_range          = 8080
     destination_port_range     = "*"
     source_address_prefix      = "*"
-    destination_address_prefix = "8000"
+    destination_address_prefix = "*"
   }
 
   tags = {
