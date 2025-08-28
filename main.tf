541f8771c10b608d029dc5b28bdf59b4615cc2a5
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-08-28 10:10:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,47 +10,6 @@ provider "azurerm" {
 }
 
 
-resource "google_storage_bucket" "test_bucket_pankhuri" {
-  project                     = "pankhuri-test-proj-436319"
-  name                        = "test-bucket-pankhuri"
-  location                    = "ASIA"
-  storage_class               = "ARCHIVE"
-  uniform_bucket_level_access = false
-}
-
-resource "google_bigquery_dataset" "default" {
-  dataset_id                  = "mydataset"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "us-east-1"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-}
-
-resource "azurerm_network_security_group" "test_network_security_group_pankhuri" {
-  name                = "test-network-security-group-pankhuri"
-  location            = "westeurope"
-  resource_group_name = "iac-rego-testing"
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = 8080
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "Production"
-  }
-}
 
 resource "google_project_iam_member" "project_editor_violation" {
   project = "pankhuri-test-proj-436319" # Replace with your project ID
