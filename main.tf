c9d49b80cfbc7e3c43fb20ee5f9a0f99baf87096
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2025-09-11 11:36:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-provider "google" {
-  project = "pankhuri-test-proj-436319"
-  region  = "us-central1"
-}
+
 provider "azurerm" {
     subscription_id = "5b87efeb-4ef9-40ca-8cb4-cfa6d2cd0351"
     tenant_id = "a162027e-b677-4249-b759-a9e6efa41922"
