c5d041d3ca6d29f4c867307e9629cda5a9ec2dee
TarangAgarwal <56721071+TarangAgarwal@users.noreply.github.com>
2025-10-27 18:05:37 +0530

    Update main.tf - testing b/446875124 (2)

--- main.tf
+++ main.tf
@@ -28,29 +28,29 @@ resource "google_kms_crypto_key" "crypto_keys" {
 #     }
 # }
 
-# resource "google_bigquery_dataset" "default" {
-#   project = "pankhuri-test-proj-436319"
-#   dataset_id                  = "mydataset"
-#   friendly_name               = "test"
-#   description                 = "This is a test description"
-#   location                    = "us-east-1"
-#   default_table_expiration_ms = 3600000
+resource "google_bigquery_dataset" "default" {
+  project = "pankhuri-test-proj-436319"
+  dataset_id                  = "mydataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "us-east-1"
+  default_table_expiration_ms = 3600000
 
-#   labels = {
-#     env = "default"
-#   }
-# }
-# resource "google_storage_bucket" "test_bucket_pankhuri" {
-#   project                     = "pankhuri-test-proj-436319"
-#   name                        = "test-bucket-pankhuri"
-#   location                    = "ASIA"
-#   storage_class               = "ARCHIVE"
-#   uniform_bucket_level_access = false
-#   logging {
-#     log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-#     log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-#   }
-# }
+  labels = {
+    env = "default"
+  }
+}
+resource "google_storage_bucket" "test_bucket_pankhuri" {
+  project                     = "pankhuri-test-proj-436319"
+  name                        = "test-bucket-pankhuri"
+  location                    = "ASIA"
+  storage_class               = "ARCHIVE"
+  uniform_bucket_level_access = false
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  }
+}
 
 # resource "azurerm_network_security_group" "test_network_security_group_pankhuri" {
 #   name                = "test-network-security-group-pankhuri"
