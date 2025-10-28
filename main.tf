1e9d10afda2fb17243668fd79ceda841af6feda7
TarangAgarwal <56721071+TarangAgarwal@users.noreply.github.com>
2025-10-28 17:41:56 +0530

    Update main.tf - demo crypto key fix (3)

--- main.tf
+++ main.tf
@@ -31,17 +31,6 @@ resource "google_bigquery_dataset" "default" {
     env = "default"
   }
 }
-resource "google_storage_bucket" "test_bucket_pankhuri" {
-  project                     = "pankhuri-test-proj-436319"
-  name                        = "test-bucket-pankhuri"
-  location                    = "ASIA"
-  storage_class               = "ARCHIVE"
-  uniform_bucket_level_access = false
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  }
-}
 
 resource "azurerm_network_security_group" "test_network_security_group_pankhuri" {
   name                = "test-network-security-group-pankhuri"
