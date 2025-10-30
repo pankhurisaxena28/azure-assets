48e4ed9ba8a23115b177da28874e0c5ca0a1469d
TarangAgarwal <56721071+TarangAgarwal@users.noreply.github.com>
2025-10-30 19:50:09 +0530

    Update main.tf - crypto key fix demo with misconfigs

--- main.tf
+++ main.tf
@@ -53,3 +53,22 @@ resource "azurerm_network_security_group" "test_network_security_group_pankhuri"
     environment = "Production"
   }
 }
+
+resource "google_storage_bucket" "test_bucket_pankhuri" {
+  project                     = "pankhuri-test-proj-436319"
+  name                        = "test-bucket-pankhuri"
+  location                    = "ASIA"
+  storage_class               = "ARCHIVE"
+  uniform_bucket_level_access = false
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  }
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "restrict-tls-opc-test-bucket"
+  location      = "US"
+  project       = "pankhuri-test-proj-436319"
+  force_destroy = true
+}
